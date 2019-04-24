524cbf34f88048ea6dfaabffc158edb5d692f551
NiceGlobals <nicefirebase@gmail.com>
2019-04-25 02:06:38 +0530

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   firebaseConfig: {
     apiKey: 'AIzaSyCiRsz0SvHP8UpD_o6C9du_nHpBcDmv6-Q',
     authDomain: 'mastitime-projects.firebaseapp.com',
-    databaseURL: 'https://mastitime-projects.firebaseio.com',
+    databaseURL: 'https://mastitime-projects-sandbox.firebaseio.com/',
     projectId: 'mastitime-projects',
     storageBucket: 'mastitime-projects.appspot.com',
     messagingSenderId: '873517723368'
